16339a5a0bd826318393b362b6fd8d09fdda9304
Edite10 <budzilkoedite@gmail.com>
2025-05-11 20:16:39 +0100

    Update admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -3,3 +3,17 @@ from .models import Table, Reservation
 
 admin.site.register(Table)
 admin.site.register(Reservation)
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'seats', 'available')
+    list_filter = ('available',)
+    ordering = ('number',)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'table', 'date', 'time')
+    list_filter = ('date', 'table')
+    search_fields = ('customer_name',)
+    ordering = ('-date', 'time')
+    
--- spa/admin.py
+++ spa/admin.py
@@ -3,3 +3,16 @@ from .models import SpaService, SpaBooking
 
 admin.site.register(SpaService)
 admin.site.register(SpaBooking)
+
+@admin.register(SpaService)
+class SpaServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration', 'price')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(SpaBooking)
+class SpaBookingAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'service', 'date', 'time')
+    list_filter = ('service', 'date')
+    search_fields = ('customer_name',)
+    ordering = ('-date', 'time')
